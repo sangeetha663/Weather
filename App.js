037d7dc45214432c48f6f6bad802322a7d6c3b49
sangeetha663 <88585853+sangeetha663@users.noreply.github.com>
2022-04-11 18:50:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Platform, StyleSheet, View, Text, ImageBackground, KeyboardAvoidingView} from 'react-native';
+
+import SearchInput from './components/searchInput'
+import getImageWeather from './utils/getImageWeather';
+export default class App extends React.Component {  
+    constructor(props) {
+      super(props);
+        this.state = {
+          location: 'San Francisco',
+      };
+    }
+
+    handleUpdateLocation = city => {
+      this.setState({
+        location: city,
+      });
+    };
+
+  render(){
+    const { location } = this.state;
+
+    return (
+      <KeyboardAvoidingView behaviour='height' style={styles.container}>
+        <ImageBackground source={getImageWeather('Clear')} style={styles.imageContainer} imageStyle={styles.image}>
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+            <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+            <SearchInput placeholder="Search any city..." onSubmit={this.handleUpdateLocation}/>
+            </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#34495E',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: '#FFF',  
+    textAlign: 'center',
+    fontFamily:
+    Platform.select({
+      ios: {
+      fontFamily: 'AvenirNext-Regular',
+      },
+      android: {
+      fontFamily: 'Roboto',
+      },
+    })
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  imageContainer: {
+    flex: 1,
+    },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
+    },
+});
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Weather
+# weatherapp![Weather-app op](https://user-images.githubusercontent.com/87593292/162618944-2b595ecc-4015-476c-b358-04ed91304196.png)
